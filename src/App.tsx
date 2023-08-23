59441107bbf3191d1aeb5557bd31dd4009afddbc
James McGauran <jamesmcgauran11@gmail.com>
2023-08-23 17:19:50 +0100

    Redo routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,42 @@
 import { Fragment } from 'react';
-import { Outlet } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import LyricsView from './components/LyricsView.tsx';
 import Navbar from './components/Navbar.tsx';
 import { UserProvider } from './context/UserContext.tsx';
+import './index.css';
+import Home from "./pages/Home.tsx";
+import PublishLyricsPage from './pages/PublishLyricsPage.tsx';
+import UserPage from "./pages/UserPage.tsx";
 
 function App() {
 
-
-
   return (
+    <BrowserRouter>
     <Fragment>
       <UserProvider>
-      <Navbar/>
-      <Outlet />
-      <ToastContainer
-        position='top-center'
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-    </UserProvider>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/lyric/:eventID' element={<LyricsView />} />
+          <Route path='/publish' element={<PublishLyricsPage />} />
+          <Route path='/user' element={<UserPage />} />
+        </Routes>
+        <ToastContainer
+          position='top-center'
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </UserProvider>
     </Fragment>
+    </BrowserRouter>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,43 +1,9 @@
 import { NDKProvider } from "@nostr-dev-kit/ndk-react"
 import React from 'react'
-import *  as ReactDOM from 'react-dom/client'
-import {
-  RouterProvider,
-  createBrowserRouter
-} from "react-router-dom"
+import * as ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import LyricsView from './components/LyricsView.tsx'
 import './index.css'
-import ErrorPage from './pages/ErrorPage.tsx'
-import PublishLyricsPage from './pages/PublishLyricsPage.tsx'
-import UserPage from "./pages/UserPage.tsx"
-import Home from "./pages/Home.tsx"
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "lyrics",
-        element: <LyricsView />,
-      },
-      {
-        path: "publish",
-        element: <PublishLyricsPage />,
-      },
-      {
-        path: "user",
-        element: <UserPage />,
-      },
-    ],
-  },
-]);
+import { EventProvider } from "./context/EventContext.tsx"
 
 // TODO : how to configure relays for prod? env ?
 
@@ -46,10 +12,12 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <NDKProvider
       relayUrls={[
         "ws://127.0.0.1:8080",
-        "wss://purplepag.es"
+        // "wss://purplepag.es"
       ]}
     >
-      <RouterProvider router={router} />
+      <EventProvider>
+        <App />
+      </EventProvider>
     </NDKProvider>
   </React.StrictMode>,
 )
