72e0d3f2568e08a203e4d5ca016417050b119e70
James McGauran <jamesmcgauran11@gmail.com>
2023-08-28 18:59:41 +0100

    Add image url input to form

--- src/pages/PublishLyricsPage.tsx
+++ src/pages/PublishLyricsPage.tsx
@@ -7,7 +7,9 @@ import { useUser } from "../context/UserContext";
 interface FormData {
   title: string;
   content: string;
-  tags: NDKTag[] // An array of arrays where each inner array contains two strings
+  image: string,
+  summary: string; 
+  tags: NDKTag[] 
 }
 
 export default function PublishLyricsPage() {
@@ -15,11 +17,11 @@ export default function PublishLyricsPage() {
   const { ndk } = useNDK();
   const { user } = useUser();
   const [loading, setLoading] = useState(false);
-  const [content, setContent] = useState("")
-  const [summary, setSummary] = useState("Lyrics posted on stargazer")
   const [formData, setFormData] = useState<FormData>({
     title: '',
     content: '',
+    image: '',
+    summary: "Lyrics posted on stargazer",
     tags: [["t", "lyrics"]]
   });
 
@@ -39,16 +41,28 @@ export default function PublishLyricsPage() {
       event.created_at = Math.floor(Date.now() / 1000);
 
       const newTags: NDKTag[] = [
-        ["summary", summary],
+        ["summary", formData.summary],
         ["title", formData.title],
         ["published_at", event.created_at.toString()]
       ];
+
+      if(formData.image.length != 0){
+        console.log("Image url provided")
+        if(isValidURL(formData.image)){
+          newTags.push(["image", formData.image])
+        } else {
+          toast.error("The image URL provided is not valid - double check it")
+          return;
+        }
+      }
 
 
       setFormData(prevFormData => ({
         ...prevFormData,
         tags: [...prevFormData.tags, ...newTags]
       }));
+
+      console.log(formData)
 
       formData.tags.map((tag)=>{
         event.tags.push(tag)
@@ -59,7 +73,7 @@ export default function PublishLyricsPage() {
       });
       
 
-      event.content = content;
+      event.content = formData.content;
       //await event.publish();
       console.log(event)
     } else {
@@ -68,9 +82,17 @@ export default function PublishLyricsPage() {
 
   }
 
+  function isValidURL(url: string) {
+    try {
+      new URL(url);
+      return true;
+    } catch (_) {
+      return false;
+    }
+  }
+
   const onChange = (e:any) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
-    console.log(formData)
   };;
 
 
@@ -92,7 +114,7 @@ export default function PublishLyricsPage() {
                 <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
                   <div className="grid grid-cols-3 gap-6">
                     <div className="col-span-3 sm:col-span-2">
-                      <label htmlFor="company-website" className="block text-sm font-medium text-gray-700">
+                      <label htmlFor="title" className="block text-sm font-medium text-gray-700">
                         Title
                       </label>
                       <div className="mt-1 flex rounded-md shadow-sm">
@@ -102,7 +124,7 @@ export default function PublishLyricsPage() {
                           required
                           name="title"
                           id="title"
-                          className="block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
+                          className="p-1 block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
                           placeholder="Title"
                         />
                       </div>
@@ -110,7 +132,7 @@ export default function PublishLyricsPage() {
                   </div>
 
                   <div>
-                    <label htmlFor="about" className="block text-sm font-medium text-gray-700">
+                    <label htmlFor="content" className="block text-sm font-medium text-gray-700">
                       Content
                     </label>
                     <div className="mt-1">
@@ -120,7 +142,7 @@ export default function PublishLyricsPage() {
                         onChange={onChange}
                         required
                         rows={24}
-                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                        className="p-1 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                         placeholder="Jesus in the day spa.."
                         defaultValue={''}
                       />
@@ -132,54 +154,21 @@ export default function PublishLyricsPage() {
 
                   <div className="grid grid-cols-3 gap-6">
                     <div className="col-span-3 sm:col-span-2">
-                      <label htmlFor="company-website" className="block text-sm font-medium text-gray-700">
+                      <label htmlFor="image" className="block text-sm font-medium text-gray-700">
                         Link to Album Art/Cover Image (Optional)
                       </label>
                       <div className="mt-1 flex rounded-md shadow-sm">
                         <input
                           type="text"
-                          name="company-website"
-                          id="company-website"
-                          className="block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
-                          placeholder="Title"
+                          name="image"
+                          id="image"
+                          onChange={onChange}
+                          className="p-1 block w-max flex-1 rounded-none rounded-r-md border-grey-900 border focus:border-indigo-500 focus:ring-indigo-500"
+                          placeholder="Link to image"
                         />
                       </div>
                     </div>
                   </div>
-
-
-                  {/* <div>
-                    <label className="block text-sm font-medium text-gray-700">Cover photo</label>
-                    <div className="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
-                      <div className="space-y-1 text-center">
-                        <svg
-                          className="mx-auto h-12 w-12 text-gray-400"
-                          stroke="currentColor"
-                          fill="none"
-                          viewBox="0 0 48 48"
-                          aria-hidden="true"
-                        >
-                          <path
-                            d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                            strokeWidth={2}
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          />
-                        </svg>
-                        <div className="flex text-sm text-gray-600">
-                          <label
-                            htmlFor="file-upload"
-                            className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
-                          >
-                            <span>Upload a file</span>
-                            <input id="file-upload" name="file-upload" type="file" className="sr-only" />
-                          </label>
-                          <p className="pl-1">or drag and drop</p>
-                        </div>
-                        <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-                      </div>
-                    </div>
-                  </div> */}
                 </div>
                 <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
                   <button
